81d7aee3fc9d1067692c7a473d7f9559d6e481cf
Jakob de Maeyer <jakobdm1@gmail.com>
2023-06-23 13:57:36 +0200

    Release v0.9.7

--- dwdparse/__init__.py
+++ dwdparse/__init__.py
@@ -3,7 +3,7 @@ from dwdparse.parsers import get_parser
 from dwdparse.stations import load_stations
 
 
-__version__ = '0.9.6'
+__version__ = '0.9.7'
 __all__ = [
    '__version__',
    'get_parser',
